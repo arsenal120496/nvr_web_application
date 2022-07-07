dc118b2201d67bfe4059f9e4837bd28b261124fe
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-07 10:08:53 +0700

    Initialize configuration file

---
+++ backend/configs/config.py
@@ -0,0 +1,31 @@
+class Config(object):
+    # all of the classes below is going to inherit these variables
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_HEADERS = 'Content-Type'
+    SESSION_COOKIE_SECURE = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    # interactive debugger will be shown for unhandled exceptions
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_HEADERS = 'Content-Type'
+    SESSION_COOKIE_SECURE = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_HEADERS = 'Content-Type'
+    SESSION_COOKIE_SECURE = False
+
+
