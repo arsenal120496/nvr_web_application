b117a30a7b10b4688195033180428ee859cfbeee
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-12 10:50:34 +0700

    Currently comment out this version of model, current model is defined and used in app.py file

---
+++ backend/models/camera.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("C:\\Users\\vinhh\\Desktop\\nvr_web_application\\backend")
+from app import db 
+
+# model này đang được define và sử dụng trong app.py
+
+# class Camera(db.Model):
+#       pass
