30edbe291b54df4d221c443083aabbfc60f923bc
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-12 10:50:44 +0700

    Currently comment out this version of model, current model is defined and used in app.py file

---
+++ backend/models/manager.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("C:\\Users\\vinhh\\Desktop\\nvr_web_application\\backend")
+from app import db
+
+# model này đang được define và sử dụng trong app.py
+
+# class Manager(db.Model):
+#   pass
