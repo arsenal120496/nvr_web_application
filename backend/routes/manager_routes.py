14c65755904417acdbaa532dd7051912958bcb3e
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-12 10:49:25 +0700

    Define route '/' for prefix route '/manager'

---
+++ backend/routes/manager_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from service.manager_service import *
+
+manager_page = Blueprint('manager_page', __name__)
+
+# all of Flask routes go in here
+@manager_page.route('/')
+def home():
+    return "day là manager"
