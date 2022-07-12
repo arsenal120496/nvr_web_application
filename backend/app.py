eb8370d3adaae8c39379c614eb011a000252e43d
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-12 10:45:46 +0700

    Define 2 models after initializing the database

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,29 @@
 from flask import Flask
-from routes.routes import simple_page
 from flask_cors import CORS
-from models.shared_model import db
+from flask_sqlalchemy import SQLAlchemy
+from init_app import init_db, init_blueprint,set_config
 
 app = Flask(__name__)
-
-# setting which configuration to use depending on the environment
-if app.config["ENV"] == "production":
-    app.config.from_object('configs.config.ProductionConfig')
-elif app.config["ENV"] == "testing":
-    app.config.from_object('configs.config.TestingConfig')
-elif app.config["ENV"] == "development":
-    app.config.from_object('configs.config.DevelopmentConfig')
-
 # Creating sqlite database and connecting blueprint routes
-db.app = app
-db.init_app(app)
-app.register_blueprint(simple_page)
-
+set_config(app)
 # prevent CORS 
 CORS(app)
 
+db = init_db(app)
+
+
+# model bắt buộc phải để ngay đây sau thằng init_db (em tìm cách define model trong file model
+# rồi import vô đây nhưng không được)
+
+# khi chạy thì mình sẽ ra database.db có chứa 2 table là Camera và Manager
+class Camera(db.Model):
+    pass
+class Manager(db.Model):
+    pass
+
+init_blueprint(app)
+
 db.create_all()
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
-
-
-
-
-
-
