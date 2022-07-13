cf990efdda7c6a6bde0345df95dc8c2a0af90554
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-13 16:32:59 +0700

    Break this file into 2 files for each model with their own purpose

--- backend/routes/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from routes.report_members import testing
-simple_page = Blueprint('simple_page', __name__)
-
-# all of Flask routes go in here
-@simple_page.route('/')
-def home():
-    testing()
-    return "day là home"
