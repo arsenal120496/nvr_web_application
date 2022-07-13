2402df3573a8cc6a031379eafc54646d30f9d83d
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-13 16:31:39 +0700

    Delete cam model for restructuring purpose

--- backend/models/cam_model.py
+++
@@ -1,9 +0,0 @@
-from models.shared_model import db
-
-class Record(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task_name = db.Column(db.String(240))
-    completed = db.Column(db.Boolean, default=False, nullable=False)
-    
-
-    
