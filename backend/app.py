4d449fa788293dae16ad6953226f8856cf1a7a1e
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-13 15:38:02 +0700

    Initialize application file that use init_app file to setup environment, initialize the database, and setup routes when the application runs

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,26 @@
 from flask import Flask
-from routes.routes import simple_page
 from flask_cors import CORS
-from models.shared_model import db
+from flask_sqlalchemy import SQLAlchemy
+from init_app import init_db, init_blueprint, set_config
+from flask_migrate import Migrate
+from sqlalchemy import MetaData
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
-# setting which configuration to use depending on the environment
-if app.config["ENV"] == "production":
-    app.config.from_object('configs.config.ProductionConfig')
-elif app.config["ENV"] == "testing":
-    app.config.from_object('configs.config.TestingConfig')
-elif app.config["ENV"] == "development":
-    app.config.from_object('configs.config.DevelopmentConfig')
-
-# Creating sqlite database and connecting blueprint routes
-db.app = app
-db.init_app(app)
-app.register_blueprint(simple_page)
-
-# prevent CORS 
+# set the configuration for the application
+app.config["ENV"] = "development"
+set_config(app)
+# prevent CORS
 CORS(app)
 
-db.create_all()
+# set up the database and model
+db = SQLAlchemy()
+migrate = Migrate()
+init_db(app)
+
+# set up the blueprint for routes that manipulate the database 
+init_blueprint(app)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
