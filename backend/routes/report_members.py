e51bfdc5cb031720bc7a6fcaa8067b3225d3e17d
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-13 16:32:30 +0700

    Delete this file because it is redundant

--- backend/routes/report_members.py
+++
@@ -1,8 +0,0 @@
-from models.cam_model import Record,db
-
-def testing():
-    record = Record(task_name="huy",completed=True)
-    db.session.add(record)
-    db.session.commit()
-    print("testing")
-    return "123"
