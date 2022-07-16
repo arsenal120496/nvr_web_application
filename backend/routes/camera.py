9a6d545cfb8ab52cc90c4f45b6b03fc14ed9e481
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-16 18:05:53 +0700

    Create camera routes to add camera and another route to query camera based on id

--- backend/routes/camera.py
+++ backend/routes/camera.py
@@ -8,3 +8,12 @@ camera_page = Blueprint('camera_page', __name__)
 def home():
     return "This is default Camera Page"
 
+@camera_page.route('/addCamera', methods=["POST", "GET"])
+def add_camera():
+    add_camera_service()
+    return "camera added successfully"
+
+@camera_page.route('/query/<id>')
+def query_camera(id):
+    item = query_camera_service(id)
+    return item
