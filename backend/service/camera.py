c749b40cddd65c10651b4ae856a71bbfc5cc8d0b
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-22 00:14:12 +0700

    Put all the code in try block, fix response return statement, useretrieve method from Camera class

--- backend/service/camera.py
+++ backend/service/camera.py
@@ -1,6 +1,4 @@
-from sqlalchemy.inspection import inspect
-from flask import jsonify
-from flask_api import status
+from flask import jsonify, Response, json
 
 # function that adds the input object to the database
 def add_to_database(object):
@@ -38,46 +36,37 @@ def change_camera_url(object, target_url):
 # function that return the list of all camera objects currently in the database
 def get_database():
     from models import Camera
-    all_camera = Camera.query.all()
-    camera_list = []
-    for i in range(len(all_camera)):
-        camera_dictionary = {}
-        for field in [c.key for c in inspect(all_camera[i]).mapper.column_attrs]:
-            data = all_camera[i].__getattribute__(field)
-            camera_dictionary[field] = data
-            camera_list.append(camera_dictionary)
-    camera_list = list({(v['camera_name'], v['rtsp_url']):v for v in camera_list}.values())
+    camera_list = Camera.retrieveList()
     return camera_list
 
 # function that add the input camera object to database
-def add_camera_service(camera_object):
+def add_camera_service(user_id, camera_name, rtsp_url):
     from models import Camera
     # check in case camera object passed in from front end is an empty object (NoneType)
     try:
-        camera_name = camera_object['camera_name']
-        rtsp_url = camera_object['rtsp_url']
-    except:
-        return jsonify({"message": "Camera object passed in value is None"}), status.HTTP_404_NOT_FOUND
-    camera_to_add = Camera(camera_name=camera_name, rtsp_url=rtsp_url)
-    # check the uniqueness of camera name and its rtsp url
-    camera_name_count = Camera.query.filter_by(camera_name=camera_name).count()
-    camera_url_count = Camera.query.filter_by(rtsp_url=rtsp_url).count()
-    if camera_name_count == 0 and camera_url_count == 0:
-        add_to_database(camera_to_add)
-        return jsonify({"message": "camera added successfully"}), status.HTTP_200_OK
-    else: 
-        return jsonify({"message": "fail to add camera"}), status.HTTP_500_INTERNAL_SERVER_ERROR
+        camera_to_add = Camera(user_id=user_id, camera_name=camera_name, rtsp_url=rtsp_url)
+        # check the uniqueness of camera name and its rtsp url
+        camera_name_count = Camera.query.filter_by(camera_name=camera_name).count()
+        camera_url_count = Camera.query.filter_by(rtsp_url=rtsp_url).count()
+        # chỗ này if với else để check nó không bị trùng trong database nên phải return 2 cái trong 
+        # block try được ko a, hay bắt buộc chỉ có 1 return statement trong block try
+        if camera_name_count == 0 and camera_url_count == 0:
+            add_to_database(camera_to_add)
+            return Response(json.dumps({"message": "camera added successfully"}), status=200, mimetype='application/json')
+        else: 
+            return Response(json.dumps({"message": "fail to add camera"}), status=500, mimetype='application/json')
+
+    except Exception as err:
+        return Response(json.dumps({"message": "error {}".format(err)}), status=404, mimetype='application/json')
+    
 
 # function that returns the information of the camera object based on input id 
 def query_camera_service(id):
     from models import Camera
-    # this replace get_or_404() method
     try: 
-        query_item = Camera.query.get(id)
+        query_camera = Camera.retrieveCamera(id)
+        return query_camera
     except:
-        return jsonify({"message": "queried camera not existed"}), status.HTTP_404_NOT_FOUND
-    converted_item = {}
-    for field in [c.key for c in inspect(query_item).mapper.column_attrs]:
-        data = query_item.__getattribute__(field)     
-        converted_item[field] = data
-    return converted_item
+        return Response(json.dumps({"message": "queried camera not existed"}), status=404, mimetype='application/json')
+
+    
