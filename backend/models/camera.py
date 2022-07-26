9a73042703b506eaab7c3112cab0e78ceb1ac720
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-26 12:16:55 +0700

    Fix the consistency of return statements

--- backend/models/camera.py
+++ backend/models/camera.py
@@ -1,7 +1,5 @@
 from requests import request
 from app import db
-from flask import Response, jsonify
-from flask_api import status
 from sqlalchemy.inspection import inspect
 
 
@@ -17,23 +15,24 @@ class Camera(db.Model):
         self.rtsp_url = rtsp_url
         
     def retrieveList():
-        all_camera = Camera.query.all()
-        camera_list = []
-        for i in range(len(all_camera)):
-            camera_dictionary = all_camera[i].toDict()
-            camera_list.append(camera_dictionary)
-        # thằng này return status 200 là nó bị cái lỗi giống hôm qua em cho anh coi
-        return camera_list
+        try: 
+            all_camera = Camera.query.all()
+            camera_list = []
+            for i in range(len(all_camera)):
+                camera_dictionary = all_camera[i].toDict()
+                camera_list.append(camera_dictionary)
+            return camera_list
+        except Exception as err:
+            return {"message": "error {}".format(err)}
 
     def retrieveCamera(id):
         try: 
             query_item = Camera.query.get(id)
             query_dictionary = Camera.toDict(query_item)
-            # thằng này return status 200 là nó bị cái lỗi giống hôm qua em cho anh coi
             return query_dictionary
-        except:
-            return Response(jsonify({"message": "queried camera not existed"}), status=404, mimetype='application/json')
-    
+        except Exception as err:
+            return {"message": "error {}".format(err)}    
+
     def update(id):
         try:
             update_item = Camera.query.get(id).first()
@@ -45,30 +44,32 @@ class Camera(db.Model):
             new_item = Camera(camera_id=id, user_id=user_id, camera_name=camera_name, rtsp_url=rtsp_url)
             db.session.add(new_item)
             db.session.commit()
-            return new_item, status.HTTP_200_OK
-        except:
-            return Response(jsonify({"message": "queried camera not existed"}), status=404, mimetype='application/json')
+            return new_item
+        except Exception as err:    
+            return {"message": "error {}".format(err)}
 
     def delete(id):
         try: 
             delete_item = Camera.query.filter_by(camera_id=id).first()
             db.session.delete(delete_item)
             db.session.commit()
-            return Response(jsonify({"message": "camera deleted successfully"}), status=200, mimetype='application/json')
-        except:
-            return Response(jsonify({"message": "queried camera not existed"}), status=404, mimetype='application/json')
+            return {"message": "camera deleted successfully"}
+        except Exception as err:
+            return {"message": "error {}".format(err)}
 
-
-    # convert a Camera object to dictionary
+    # convert a Camera object to dictionary, return empty dictionary if 
     def toDict(camera_object):
         try:
             converted_item = {}
-            for field in [c.key for c in inspect(camera_object).mapper.column_attrs]:
-                data = camera_object.__getattribute__(field)     
-                converted_item[field] = data
-            return converted_item
-        except Exception as err:
-            return Response(jsonify({"message": "error {}".format(err)}), status=404, mimetype='application/json')
+            if camera_object is None:
+                return {}
+            else:
+                for field in [c.key for c in inspect(camera_object).mapper.column_attrs]:
+                    data = camera_object.__getattribute__(field)     
+                    converted_item[field] = data
+                return converted_item
+        except Exception as err: 
+            return {"message": "error {}".format(err)}
 
 
     
