c889946cbf86e6ecfb64f79c4fd1b6897cac00b9
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-07-26 12:16:28 +0700

    Cover more unexpected error cases, fix the consistency of return statements

--- backend/routes/camera.py
+++ backend/routes/camera.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 from service.camera import *
-camera_page = Blueprint('camera_page', __name__)
 import json
+
+camera_page = Blueprint('camera_page', __name__)
 
 # all of Flask routes that use Camera services go in here
 @camera_page.route('/')
@@ -9,30 +10,46 @@ def home():
     return "This is default Camera Page"
 
 # Data format we need from UI: json
-# json format: {camera_name: "example", rtsp_url: "www.example.com"}
+# json format: {camera_name: "example", rtsp_url: "www.example.com", user_id: 1}
 # tested with postman (succesfully added json data to database)
 @camera_page.route('/addCamera', methods=["POST"])
 def add_camera():
-    camera_object = request.json
-    camera_name = camera_object['camera_name']
-    rtsp_url = camera_object['rtsp_url']
-    user_id = camera_object['user_id']
-    if (camera_name is not None) and (rtsp_url is not None) and (user_id is not None):
-        add_camera_status = add_camera_service(user_id, camera_name, rtsp_url)
-    else:
-        return Response(json.dumps({"message": "camera object is NoneType"}), status=404, mimetype='application/json')
-    return add_camera_status
+    try: 
+        camera_object = request.json
+        camera_name = camera_object['camera_name']
+        rtsp_url = camera_object['rtsp_url']
+        user_id = camera_object['user_id']
+        # check if the json data passed in from UI is not None 
+        if (camera_name is not None) and (rtsp_url is not None) and (user_id is not None):
+            add_camera_status = add_camera_service(user_id, camera_name, rtsp_url)
+            # check if the Camera object already exist in the database
+            if add_camera_status == {}:
+                return Response(json.dumps({"message": "fail to add camera, already exist in the database"}), status=412, mimetype='application/json')
+            else:
+                return Response(json.dumps(add_camera_status), status=200, mimetype='application/json')
+        else:
+            return Response(json.dumps({"message": "camera object is NoneType"}), status=404, mimetype='application/json')
+    except Exception as err:
+        return Response(json.dumps({"message": "fail to add camera, error {}".format(err)}), status=404, mimetype='application/json')
 
 # return json data format of camera information depending on id to UI
 @camera_page.route('/query/<id>', methods=["GET"])
-def query_camera(id):  
-    query_result = query_camera_service(id)
-    json_result = json.dumps(query_result)
-    return json_result
-    
+def query_camera(id):
+    try:   
+        query_result = query_camera_service(id)
+        if (query_result is None or query_result == {}):
+            return Response(json.dumps({"message": "no camera object at given id"}),status=203, mimetype='application/json')
+        else: 
+            return Response(json.dumps(query_result), status=200, mimetype='application/json')
+    except Exception as err:
+        return Response(json.dumps({"message": "error {}".format(err)}), status=404, mimetype='application/json')
+        
 # return json data format of list of all cameras information 
 @camera_page.route('/getList', methods=["GET"])
 def get_list():
-    database_list = get_database()
-    database_json = json.dumps(database_list)
-    return database_json
+    try:
+        database_list = get_database()
+        database_json = json.dumps(database_list)
+        return Response(database_json, status=200, mimetype='application/json')
+    except Exception as err:
+        return Response(json.dumps({"message": "error {}".format(err)}), status=404, mimetype="application/json")
