8d5a8203d734b25584c8b2e30710ea7724138067
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-08-15 17:24:24 +0700

    Add logging statements to update log files when using services

--- backend/service/camera.py
+++ backend/service/camera.py
@@ -1,11 +1,13 @@
+import logging
 # function that adds the input object to the database
 def add_to_database(object):
     try: 
         from app import db
         db.session.add(object)
         db.session.commit()
+        logging.info("camera added successfully")
     except Exception as err:
-        return {"message": "fail to add camera object, err {}".format(err)}
+        logging.warning("fail to add camera object, err {}".format(err))
 
 # function that deletes the input object to the database
 def delete_from_database(object):
@@ -16,8 +18,9 @@ def delete_from_database(object):
         delete_item = Camera.query.get(item_id)
         db.session.delete(delete_item)
         db.session.commit()
+        logging.info("camera deleted successfully")
     except Exception as err:
-        return {"message": "fail to delete camera object, err {}".format(err)}
+        logging.warning("fail to delete camera object, err {}".format(err))
     
 # function that change the input object's camera name to target name
 def change_camera_name(object, target_name):
@@ -28,8 +31,9 @@ def change_camera_name(object, target_name):
         update_item = Camera.query.get(item_id)
         update_item.camera_name = target_name
         db.session.commit()
+        logging.info("camera's name updated successfully")
     except Exception as err:
-        return {"message": "fail to update camera object, err {}".format(err)}
+        logging.warning("fail to update camera's name, err{}".format(err))
 
 # function that change the input object's camera url to target url
 def change_camera_url(object, target_url):
@@ -40,17 +44,20 @@ def change_camera_url(object, target_url):
         update_url = Camera.query.get(item_id)
         update_url.rtsp_url = target_url
         db.session.commit()
+        logging.info("camera's url updated successfully")
     except Exception as err:
-        return {"message": "fail to update camera's rtsp url, err {}".format(err)}
+        logging.warning("fail to update camera's url, err{}".format(err))
 
 # function that return the list of all camera objects currently in the database
 def get_database():
     try:
         from models import Camera
         camera_list = Camera.retrieveList()
+        logging.info("database retrieved successfully")
         return camera_list
     except Exception as err:
-        return {"message": "fail to retrive camera list from database, err {}".format(err)}
+        logging.warning("fail to retrieve database information, err{}".format(err))
+        return {"message": "fail to retrieve database information, err {}".format(err)}
 
 # function that add the input camera object to database
 def add_camera_service(user_id, camera_name, rtsp_url):
@@ -65,9 +72,11 @@ def add_camera_service(user_id, camera_name, rtsp_url):
             add_to_database(camera_to_add)
             return {"message": "camera added successfully"}
         else: 
+            logging.warning("camera already existed in the database")
             return {}
     except Exception as err:
-        return {"message": "error {}".format(err)}
+        logging.warning("fail to add camera to database, err {}".format(err))
+        return {"message": "fail to add camera to database, err {}".format(err)}
     
 
 # function that returns the information of the camera object based on input id 
@@ -75,7 +84,9 @@ def query_camera_service(id):
     from models import Camera
     try: 
         query_camera = Camera.retrieveCamera(id)
+        logging.info("camera queried successfully")
         return query_camera
     except Exception as err:
-        return {"message": "error {}".format(err)}
+        logging.warning("fail to query camera's information, err {}".format(err))
+        return {"message": "fail to query camera's information, err {}".format(err)}
     
