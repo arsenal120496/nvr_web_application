8dc123f0fe11732d8146ea137547463b59db811e
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-08-17 09:34:37 +0700

    Fix minor changes

--- backend/init_app.py
+++ backend/init_app.py
@@ -1,7 +1,8 @@
 from routes.camera import camera_page
 from routes.record import record_page
 from routes.user import user_page
-
+import logging
+from logging.handlers import RotatingFileHandler
 def set_config(app):
     # setting which configuration to use depending on the environment
     if app.config["ENV"] == "production":
@@ -15,6 +16,9 @@ def init_db(app):
     # initialize the database and setup models
     from app import db  
     from models import Camera, Record, User
+    from models import Camera
+    from models import Record
+    from models import User
     db.init_app(app)
     with app.app_context():
         db.create_all()
@@ -26,16 +30,13 @@ def init_blueprint(app):
         app.register_blueprint(record_page, url_prefix='/record')
         app.register_blueprint(user_page, url_prefix='/user')
 
-class CameraObject:
-    def __init__(self):
-        self.queue = [20]
-    
-    def update_frame(self, frame):
-        print("update frame")
-        if len(self.queue) > 20:
-            self.queue.pop(0) 
-        self.queue.append(frame)
-    
-    def get_frame(self):
-        return self.queue[0]
+def config_log(app):
+    # setup logging config file for service files tracking history
+    # maximum number of files is 10, each file is 5MB maximum
+    logging.basicConfig(
+        handlers=[RotatingFileHandler('record.log', maxBytes=5*1024, backupCount=10)],
+        level=logging.INFO,
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",    
+        datefmt='%Y-%m-%dT%H:%M:%S')
+    return "initializing logging levels"
 
