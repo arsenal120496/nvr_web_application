80aed023b2ba564c1cf3664ab424cee1b6ffc006
Huy Nguyen <vinhhuynguyen123@gmail.com>
2022-08-17 09:35:13 +0700

    update to avoid merge conflicts

--- backend/models/camera.py
+++ backend/models/camera.py
@@ -1,7 +1,7 @@
-from requests import request
 from app import db
 from sqlalchemy.inspection import inspect
-
+import logging
+from database.database_manipulator import Database
 
 class Camera(db.Model):
     camera_id = db.Column(db.Integer, primary_key=True)
@@ -21,40 +21,43 @@ class Camera(db.Model):
             for i in range(len(all_camera)):
                 camera_dictionary = all_camera[i].toDict()
                 camera_list.append(camera_dictionary)
+            logging.info("list of all cameras retrieved successfully")
             return camera_list
         except Exception as err:
+            logging.warning("unable to retrieve list, error {}".format(err))
             return {"message": "error {}".format(err)}
 
     def retrieveCamera(id):
         try: 
             query_item = Camera.query.get(id)
             query_dictionary = Camera.toDict(query_item)
+            logging.info("camera object retrieved successfully")
             return query_dictionary
         except Exception as err:
+            logging.warning("unable to retrieve cammera object, error {}".format(err))
             return {"message": "error {}".format(err)}    
 
-    def update(id):
+    def update(id, updating_item):
         try:
-            update_item = Camera.query.get(id).first()
-            db.session.delete(update_item)
-            db.session.commit()
-            camera_name = request.json['camera_name']
-            rtsp_url = request.json['rtsp_url']
-            user_id = request.json['user_id']
-            new_item = Camera(camera_id=id, user_id=user_id, camera_name=camera_name, rtsp_url=rtsp_url)
-            db.session.add(new_item)
-            db.session.commit()
-            return new_item
-        except Exception as err:    
+            object = Camera.query.get(id)
+            object.user_id = updating_item['user_id']
+            object.camera_name = updating_item['camera_name']
+            object.rtsp_url = updating_item['rtsp_url']
+            Database.save()
+            logging.info("camera object updated successfully")
+            return {"message": "item updated successfully"}
+        except Exception as err:   
+            logging.warning("unable to update camera object, err {}".format(err)) 
             return {"message": "error {}".format(err)}
 
     def delete(id):
         try: 
             delete_item = Camera.query.filter_by(camera_id=id).first()
-            db.session.delete(delete_item)
-            db.session.commit()
+            Database.delete(delete_item)        
+            logging.info("camera object deleted successfully")
             return {"message": "camera deleted successfully"}
         except Exception as err:
+            logging.warning("unable to delete camera object, err {}".format(err))
             return {"message": "error {}".format(err)}
 
     # convert a Camera object to dictionary, return empty dictionary if 
@@ -62,13 +65,16 @@ class Camera(db.Model):
         try:
             converted_item = {}
             if camera_object is None:
+                logging.info("camera object is NoneType")
                 return {}
             else:
                 for field in [c.key for c in inspect(camera_object).mapper.column_attrs]:
                     data = camera_object.__getattribute__(field)     
                     converted_item[field] = data
+                logging.info("camera object converted to dictionary successfully")
                 return converted_item
         except Exception as err: 
+            logging.warning("unable to convert camera object to dictionary, err {}".format(err))
             return {"message": "error {}".format(err)}
 
 
